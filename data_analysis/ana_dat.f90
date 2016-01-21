8f1d3fd98a150bf1a839be5ee2e12998f21d204d
madorisi@ictp <madorisi@ictp.it>
2016-01-21 18:11:03 +0100

    updating data analysis code

--- data_analysis/ana_dat.f90
+++ data_analysis/ana_dat.f90
@@ -10,9 +10,9 @@ program ana_dat
   real(4) :: tmp(3*npoints)
   real(4) :: points(npoints,3)
   real(4) :: t(npoints-1,3),avg_t(npoints-1,3)
-  real(4) :: avgjmp, normt
+  real(4) :: avgjmp, normt,avg_Ree,Ree,Rcm(3),avg_Rg2
   character(100) :: arg1,main_input_dir
-  real(8) :: avg_l(npoints-1), avg_costheta(npoints-1), avg_dz(npoints-1), avg1_avgi(npoints-1)
+  real(8) :: avg_L(npoints-1), avg_costheta(npoints-1), avg_dz(npoints-1), avg1_avgi(npoints-1)
 
   call getarg(1,arg1)
   read(arg1,*), avgjmp
@@ -20,10 +20,15 @@ program ana_dat
   main_input_dir = '/scratch/madorisi/results/polymers/avjmp_'//trim(arg1)//'/'
   !main_output_dir = '../output/'
   print*, 'working on ',trim( main_input_dir )
-  avg_l = 0.
+  
+
+  avg_Ree = 0.
+  avg_Rg2 = 0.
+  avg_L = 0.
   c = 0
+  
+  
   do f = 0,nreps-1
-     !print*, f
      !call stat(trim(main_input_dir)//'rep_'//trim( int_2_str(f) )//'.dat', values) 
      !fsize=values(8)
      
@@ -43,26 +48,20 @@ program ana_dat
      close(10)
 
      
-     
+     !--------------------
+     !
+     !--------------------
      points = reshape(tmp,[npoints,3],order=[2,1])
      
-     
-     
-     do i = 1,npoints-1
-        
-        normt = norm2( points(i+1,:) - points(i,:) )
-        !t(i,:) = ( points(i+1,:) - points(i,:) )/normt     !versor between two consecutive points
-        avg_l(i) = avg_l(i) + normt/(nreps-1)
-        avg_dz(i) = avg_dz(i) + ( points(i+1,3) - points(i,3) )/(nreps-1) !jump on z direction
-     
-     end do
-     
-     ! do i=1,npoints-1
-     !    avg_t(i,:) = avg_t(i,:) + t(i,:)   
-     ! end do
-     
-     
-     !avg_costheta = avg_costheta + costheta(t,npoints) 
+     !average end to end distance 
+     avg_Ree = avg_Ree + norm2(  points(1,:) - points(npoints,:)  )/nreps
+     !center of mass position
+     Rcm = sum( points,dim=1 )/npoints 
+     avg_Rg2 = avg_Rg2 + Rg2(points,Rcm) / nreps
+     !contour length up to n-th point
+     avg_L = avg_L + L(points) / nreps
+     !distance travelled on the z-axis up to n-th point
+     avg_Lz = avg_Lz + Lz(points) / nreps
      
   end do
 
@@ -73,11 +72,11 @@ program ana_dat
 
   open(newunit=file_results,file='/scratch/madorisi/results/polymers/results_'//trim( arg1 )//'.dat',status='replace')
   do i=1,npoints-1
-    write(file_results,*), avg_l(i)/(nreps-c), avg_dz(i)/(nreps-c) !( avg_costheta(i) - avg1_avgi(i)/(nreps - c) ) / ( avg_costheta(1) - avg1_avgi(1)/(nreps-c) )
+    write(file_results,*), avg_L(i)/(nreps-c), avg_dz(i)/(nreps-c) !( avg_costheta(i) - avg1_avgi(i)/(nreps - c) ) / ( avg_costheta(1) - avg1_avgi(1)/(nreps-c) )
   end do
   close(file_results)
 
-  write(6,*), c, ' files with zero bytes ', arg1 !6=stdout
+  !write(6,*), c, ' files with zero bytes ', arg1 !6=stdout
   
 
 contains
@@ -94,20 +93,88 @@ contains
   end function int_2_str
   
 
-  function costheta(t,npoints)
+  ! function costheta(t,npoints)
+  !   implicit none
+
+  !   real,intent(in) :: t(:,:)
+  !   integer, intent(in) :: npoints
+  !   real :: costheta(npoints-1)
+    
+  !   integer :: j
+        
+  !   do i = 1,npoints-1
+  !      costheta(i) = dot_product( t(1,:) , t(i,:) )  
+  !   end do
+        
+  ! end function costheta
+
+
+  function Rg2(points,Rcm)
     implicit none
 
-    real,intent(in) :: t(:,:)
-    integer, intent(in) :: npoints
-    real :: costheta(npoints-1)
+    real, intent(in) :: points(:,:), Rcm(3)
+    real :: Rg2
+    integer :: i
     
-    integer :: j
-        
-    do i = 1,npoints-1
-       costheta(i) = dot_product( t(1,:) , t(i,:) )  
+    Rg2 = 0.
+    do i=1,size(points,1)
+       Rg2 = Rg2 + dot_product(points(i,:),points(i,:))
     end do
-        
-  end function costheta
+    
+    Rg2 = Rg2/size(points,1)
+    Rg2 = Rg2 - dot_product(Rcm,Rcm)
+
+  end function Rg2
+
+  ! function contour(points,upto)
+  !   implicit none
+
+  !   real, intent(in) :: points(:,:)
+  !   integer, intent(in) :: upto
+  !   real :: contour
+  !   integer :: i
+    
+  !   contour = 0.
 
 
+  !   do i=1,upto
+  !      contour = contour + norm2( points(i+1,:) - points(i,:) )
+  !   end do
+    
+    
+
+
+  ! end function contour
+  function L(points)
+    implicit none
+    
+    real, intent(in) :: points(:,:)
+    real :: L(size(points,1)-1)
+    
+    L = 0.
+    L(1) = norm2(points(2,:) - points(1,:))
+    
+    do i = 1,npoints-2
+       L(i+1) = L(i) + norm2( points(i+2,:) - points(i+1,:) )
+    end do
+    
+  end function L
+  
+  function Lz(points)
+    implicit none
+    !compute the jump along z 
+    !Lz(i): jump on z up to the i-th jump
+
+    real, intent(in) :: points(:,:)
+    real :: Lz(size(points,1)-1)
+    
+    Lz = 0.
+    Lz(1) = norm2(points(2,3) - points(1,3))
+    
+    do i = 1,npoints-2
+       Lz(i+1) = Lz(i) + norm2( points(i+2,:) - points(i+1,:) )
+    end do
+    
+  end function Lz
+  
 end program ana_dat
