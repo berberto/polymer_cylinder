90785b54bef3e8e47e6ec16704d17e277f22285f
madorisi@ictp <madorisi@ictp.it>
2016-01-22 18:53:11 +0100

    still on anadat code

--- data_analysis/ana_dat.f90
+++ data_analysis/ana_dat.f90
@@ -7,12 +7,17 @@ program ana_dat
   integer :: seed
   integer(4) :: values(13),file_results
   integer(4) :: f,i,blocksize,fsize,c
+  logical :: exist
   real(4) :: tmp(3*npoints)
   real(4) :: points(npoints,3)
   real(4) :: t(npoints-1,3),avg_t(npoints-1,3)
   real(4) :: avgjmp, normt,avg_Ree,Ree,Rcm(3),avg_Rg2
   character(100) :: arg1,main_input_dir
-  real(8) :: avg_L(npoints-1), avg_costheta(npoints-1), avg_dz(npoints-1), avg1_avgi(npoints-1)
+  real(4) :: avg_L(npoints-1),& !cumulative total length
+       avg_Lz(npoints-1),& !cumulative jump on z
+       tot_contour,&
+       avg_dz(npoints-1),&  !z jump
+       avg_dl(npoints-1) !jump in space
 
   call getarg(1,arg1)
   read(arg1,*), avgjmp
@@ -22,9 +27,13 @@ program ana_dat
   print*, 'working on ',trim( main_input_dir )
   
 
-  avg_Ree = 0.
-  avg_Rg2 = 0.
-  avg_L = 0.
+  avg_Ree = 0. !scalar
+  avg_Rg2 = 0. !scalar
+  avg_L = 0. ! array
+  avg_Lz = 0. !array
+  avg_dz = 0.
+  avg_dl = 0.
+  tot_contour = 0.
   c = 0
   
   
@@ -53,28 +62,39 @@ program ana_dat
      !--------------------
      points = reshape(tmp,[npoints,3],order=[2,1])
      
+     ! do i=1,npoints-1
+     !    tot_contour = tot_contour + norm2(points(i+1,:) - points(i,:))
+     ! end do
+
+     ! avg_L = L(points)
+     ! do i =1,size(avg_L,1)
+     !    print*, avg_L(i)
+     ! end do
+     ! print*, tot_contour, avg_L(npoints-1)
+     ! stop
+
      !average end to end distance 
      avg_Ree = avg_Ree + norm2(  points(1,:) - points(npoints,:)  )/nreps
      !center of mass position
      Rcm = sum( points,dim=1 )/npoints 
+     !gyration radius
      avg_Rg2 = avg_Rg2 + Rg2(points,Rcm) / nreps
      !contour length up to n-th point
      avg_L = avg_L + L(points) / nreps
      !distance travelled on the z-axis up to n-th point
      avg_Lz = avg_Lz + Lz(points) / nreps
+     avg_dz = avg_dz + dz(points) / nreps
+     avg_dl = avg_dl + dl(points) / nreps
      
   end do
 
   ! do i=1,npoints-1
   !    avg1_avgi(i) = dot_product( avg_t(1,:), avg_t(i,:) )
   ! end do
-     
+  call write_results(arg1,npoints,avg_L,avg_Lz,avg_dl,avg_dz,avg_Rg2,avg_Ree)
 
-  open(newunit=file_results,file='/scratch/madorisi/results/polymers/results_'//trim( arg1 )//'.dat',status='replace')
-  do i=1,npoints-1
-    write(file_results,*), avg_L(i)/(nreps-c), avg_dz(i)/(nreps-c) !( avg_costheta(i) - avg1_avgi(i)/(nreps - c) ) / ( avg_costheta(1) - avg1_avgi(1)/(nreps-c) )
-  end do
-  close(file_results)
+  
+  print*, trim( main_input_dir ),' done'
 
   !write(6,*), c, ' files with zero bytes ', arg1 !6=stdout
   
@@ -169,12 +189,88 @@ contains
     real :: Lz(size(points,1)-1)
     
     Lz = 0.
-    Lz(1) = norm2(points(2,3) - points(1,3))
+    Lz(1) = points(2,3) - points(1,3)
     
     do i = 1,npoints-2
-       Lz(i+1) = Lz(i) + norm2( points(i+2,:) - points(i+1,:) )
+       Lz(i+1) = Lz(i) + points(i+2,3) - points(i+1,3) 
     end do
     
   end function Lz
   
+  function dz(points)
+    implicit none
+    !compute the jump along z 
+    !Lz(i): jump on z up to the i-th jump
+
+    real, intent(in) :: points(:,:)
+    real :: dz(size(points,1)-1)
+    
+    dz = 0.
+    
+    do i = 1,npoints-1
+       dz(i) = points(i+1,3) - points(i,3) 
+    end do
+    
+  end function dz
+  
+  function dl(points)
+    implicit none
+    !compute the jump along z 
+    !Lz(i): jump on z up to the i-th jump
+
+    real, intent(in) :: points(:,:)
+    real :: dl(size(points,1)-1)
+    
+    dl = 0.
+    
+    do i = 1,npoints-1
+       dl(i) = norm2( points(i+1,:) - points(i,:) ) 
+    end do
+    
+  end function dl
+  
+
+  subroutine write_results(label,npoints,avg_L,avg_Lz,avg_dl,avg_dz,avg_Rg2,avg_Ree)
+    
+    implicit none
+
+    integer, intent(in) :: npoints 
+    character(len=100) :: label
+    real(4), intent(in) :: avg_L(npoints-1), avg_Lz(npoints-1), avg_dl(npoints-1), avg_dz(npoints-1)
+    real(4), intent(in) :: avg_Ree,avg_Rg2
+    real(4) :: avgjmp
+    integer :: i, file_results
+
+
+    !read(arg1,*), avgjmp
+
+
+    open(newunit=file_results,file='/scratch/madorisi/results/polymers/results_L_Lz_'//trim( label )//'.dat',status='unknown')
+    write(file_results,*), 'avg_L ', 'avg_Lz ', 'avg_dl ', 'avg_dz ', 'avgjmp'
+    do i=1,npoints-1
+       write(file_results,*), avg_L(i), avg_Lz(i), avg_dl(i), avg_dz(i), trim(label)!avgjmp
+    end do
+    close(file_results)
+
+
+    inquire (file='/scratch/madorisi/results/polymers/results_Ree_Rg.dat', exist=exist)
+    if (exist) then
+       open(newunit=file_results, file='/scratch/madorisi/results/polymers/results_Ree_Rg.dat',&
+            status="old", position="append", action="write")
+       write(file_results,*), avg_Rg2, avg_Ree, avg_L(npoints-1), trim(label)!avgjmp
+       close(file_results)
+    else
+       !create file and write the first time
+       open(newunit=file_results, file='/scratch/madorisi/results/polymers/results_Ree_Rg.dat', status="new", action="write")
+       write(file_results,*), '# avgRg2 ','avgRee ','avgL ','avgjmp'
+       write(file_results,*), avg_Rg2, avg_Ree, avg_L(npoints-1), trim(label)!avgjmp
+       close(file_results)
+    end if
+
+
+
+
+  end subroutine write_results
+
+
 end program ana_dat
